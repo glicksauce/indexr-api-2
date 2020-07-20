f943c6fa6af36761760e6a3d1215f60b8a8f80fa
Jay Glickman <jayglickman@gmail.com>
2020-07-20 19:29:30 -0400

    limited routes to increase security

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :album_tags
+  # resources :album_tags
   # resources :tags
   # resources :albums
-  resources :users, only: [:index, :show, :create] do
+  resources :users, only: [:create] do
     get 'user_album_tags', to: 'album_tags#user_album_tags', as: :user_album_tags
     resources :album_tags, only: [:user_album_tags]
 
